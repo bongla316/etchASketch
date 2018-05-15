7bb71e3cac8404a5b9db5373b6197d0f0b587ee3
bongla316 <abdullah.safdar@gmail.com>
2018-05-15 19:50:15 +0100

    Added functionality for a slider to adjust the grid size

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+document.addEventListener("DOMContentLoaded", createDivs(16));
+document.querySelector('#clearGrid').addEventListener("click", clearGrid);
+
 function calculateBoxSize(size) {
-    console.log(Math.round(600/size));
-    return Math.round(600/size);
+    console.log(Math.round(700/size)-2);
+    return Math.round(700/size)-2;
 }
 
 function createDivs(gridSize) {
-    var container = document.getElementById("container");
+    document.getElementById("gridSizeSlider").value = gridSize;
+
+    var container = document.getElementById("grid");
     var i = 0;
     var j = 0;
     var number = 0;
     
-    for(i=0; i <gridSize; i++) {
+    for(i=0; i < gridSize; i++) {
         var nodeRow = document.createElement("div");
         nodeRow.setAttribute("class", "row");
         container.appendChild(nodeRow);
-
+        
         for (j=0; j < gridSize; j++) {
             var nodeData = document.createElement("div");
             nodeData.setAttribute("class", "col");
@@ -36,7 +41,8 @@ function clearGrid(){
     });
 }
 
-var gridSize = prompt("Enter grid size: ");
-
-createDivs(gridSize);
-document.querySelector('#clearGrid').addEventListener("click", clearGrid);
+document.querySelector("#gridSizeSlider").addEventListener("input", function() {
+    var newGridSize = this.value;
+    document.getElementById("grid").innerHTML = "";
+    createDivs(newGridSize);
+});
